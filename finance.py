31156af0448fef00d44c16dcbaa0f525ba2ec37e
KIM_SEONG_HUN <105344437+HUODINY@users.noreply.github.com>
2022-08-10 16:15:51 +0900

    Update finance.py

--- finance.py
+++ finance.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
-import streamlit as st
-import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
-import koreanize_matplotlib
+import FinanceDataReader as fdr
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense, Activation
+import datetime
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="반포자이까지 한걸음",
+    page_icon= "chart_with_upwards_trend",
+    layout="wide",
+)
+
+st.sidebar.markdown("# Predict stocks 📊")
 
 Stockcode = pd.read_csv('data/Stockcode.csv')
 Stockcode.set_index('Name', inplace=True)
@@ -14,30 +25,57 @@ if Name in Code_name_list:
     code_num = Stockcode.at[Name, 'Symbol']
     df = fdr.DataReader(code_num)
     df = df.rename(columns={'Open':'시가', 'High':'고가','Low':'저가', 'Close':'종가', 'Volume':'거래량', 'Change':'전일대비'})
-    col1, col2, col3 = st.columns(3)
-    col1.metric("현재 주식가격",format(df['종가'].tail(1)[0], ',')+'원', "%d원" %(df['종가'].diff().tail(1)[0]))
-    col2.metric("현재 거래량", format(df['거래량'].tail(1)[0], ','),"%.2f%%" %(df['거래량'].pct_change().tail(1)[0] * 100))
-    col3.metric("전일 대비 가격", round(df['전일대비'].tail(1)[0], 4), "%.2f%%" %(df['전일대비'].tail(1)[0] * 100))
+
+    high_prices = df['고가'].values
+    low_prices = df['저가'].values
+    mid_prices = (high_prices + low_prices) / 2
+
+    seq_len = 50
+    sequence_length = seq_len + 1
+    result = []
+    for index in range(len(mid_prices) - sequence_length):
+        result.append(mid_prices[index: index + sequence_length])
+
+    normalized_data = []
+    for window in result:
+        normalized_window = [((float(p) / float(window[0])) - 1) for p in window]
+        normalized_data.append(normalized_window)
+    result = np.array(normalized_data)
+    row = int(round(result.shape[0] * 0.9))
+    train = result[:row, :]
+    np.random.shuffle(train)
+    x_train = train[:, :-1]
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    y_train = train[:, -1]
+
+    x_test = result[row:, :-1]
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    y_test = result[row:, -1]
+
+    x_train.shape, x_test.shape
+
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=(50, 1)))
+    model.add(LSTM(64, return_sequences=False))
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mse', optimizer='rmsprop')
+    model.summary()
+
+    model.fit(x_train, y_train,
+        validation_data=(x_test, y_test),
+        batch_size=10,
+        epochs=10)
+
+    pred = model.predict(x_test)
     fig = plt.figure(facecolor='white', figsize=(20, 10))
-    plt.plot(df['종가'])
-    plt.title(Name)
+    ax = fig.add_subplot(111)
+    ax.plot(y_test, label='True')
+    ax.plot(pred, label='Prediction')
+    ax.legend()
     st.pyplot(fig)
+
 elif Name not in Code_name_list:
     st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
-
-    
-    # FX 환율, 1995 ~ 현재
-usdkrw = fdr.DataReader('USD/KRW', '1995-01-01') # 달러 원화
-usdeur = fdr.DataReader('USD/EUR', '1995-01-01') # 달러 유로화
-usdcny = fdr.DataReader('USD/CNY', '1995-01-01') # 달러 위엔화
-
-# 스트림릿 템플릿
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['usdkrw[[Close]]', 'usdeur[[Close]]', 'usdcny[[Close]]'])
-
-st.line_chart(chart_data)
-
 """유선님 """
 """
 fig = px.line(df, y='종가', title='{}의 종가(close) Time Series'.format(Name))
